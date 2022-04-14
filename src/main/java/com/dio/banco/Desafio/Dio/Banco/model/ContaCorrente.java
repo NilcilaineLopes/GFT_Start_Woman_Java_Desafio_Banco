bb0df18b90c632d08acf5bd70ae8925848c8ce47
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 19:34:05 -0300

    Adiciona imports e anotações

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaCorrente.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaCorrente.java
@@ -1,5 +1,13 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ContaCorrente {
     private int agencia;
     private int numero;
