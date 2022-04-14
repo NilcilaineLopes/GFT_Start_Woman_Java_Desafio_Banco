51dda981e4e89047453ad0ebe8dc12940ad497bd
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 19:34:20 -0300

    Adiciona imports e anotações

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
@@ -1,5 +1,13 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ContaPoupanca {
     private int agencia;
     private int numero;
