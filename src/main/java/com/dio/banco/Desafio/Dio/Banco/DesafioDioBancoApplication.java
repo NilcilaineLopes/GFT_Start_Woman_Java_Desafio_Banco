18eb7865d24b44f35d97f1700f8642e4f7333c5a
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 18:09:03 -0300

    Adiciona src

---
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/DesafioDioBancoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.banco.Desafio.Dio.Banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/banco/Desafio/Dio/Banco/DesafioDioBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.banco.Desafio.Dio.Banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
