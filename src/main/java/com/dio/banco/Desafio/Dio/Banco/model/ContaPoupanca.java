b859f2e42cec4d6d9ff4f5f02bf0c1ff822d2ed8
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 19:21:29 -0300

    Adiciona metodos de sacar, depositar e tranferir

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
@@ -4,4 +4,8 @@ public class ContaPoupanca {
     private int agencia;
     private int numero;
     private double saldo;
+
+    public void sacar(){}
+    public void depositar(){}
+    public void tranferir(){}
 }
