db4f5d808b2240664dbe0fd11d25fb9c78d6dd6b
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 19:43:01 -0300

    Atualiza classe para uma classe filha

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
@@ -3,17 +3,10 @@ package com.dio.banco.Desafio.Dio.Banco.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class ContaPoupanca {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaPoupanca extends Conta{
 
-    public void sacar(){}
-    public void depositar(){}
-    public void tranferir(){}
 }
