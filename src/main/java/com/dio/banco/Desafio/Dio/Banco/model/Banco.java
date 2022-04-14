8698c61a267804f0e1505b1053d7dbe66f7a1049
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-14 19:33:19 -0300

    Adiciona imports e anotações

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
@@ -1,7 +1,14 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Banco {
     public String nome;
 }
