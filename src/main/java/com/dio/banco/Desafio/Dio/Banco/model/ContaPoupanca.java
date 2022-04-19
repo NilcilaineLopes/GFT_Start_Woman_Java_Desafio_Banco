d3fb0837da0fdd7f978d2830342c39699603de7d
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 17:48:05 -0300

    Atualiza e adiciona metodo super

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
@@ -1,12 +1,10 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class ContaPoupanca extends Conta{
 
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int agencia, int numero) {
+        super(agencia, numero);
+    }
 }
