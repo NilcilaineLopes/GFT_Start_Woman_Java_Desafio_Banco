0a9b2ee357eec4f7a7f5cb79e527b53e0e3ee0e6
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 17:46:23 -0300

    Atualiza e adiciona get

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Conta.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Conta.java
@@ -1,15 +1,19 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
 public abstract class Conta implements IConta {
+
+    protected static final int AGENCIA_PADRAO = 1;
+    protected static  int SEQUENCIAL = 1;
+
+
     private int agencia;
     private int numero;
     private double saldo;
+
+    public Conta(int agencia, int numero) {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {
@@ -24,5 +28,17 @@ public abstract class Conta implements IConta {
     @Override
     public void tranferir(double valor, Conta contaDestino) {
 
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
