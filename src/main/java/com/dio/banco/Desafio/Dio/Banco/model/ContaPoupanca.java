15f494f20b37afac6dd08277e99a2864c3998940
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 19:34:52 -0300

    Adciona cliente em conta poupança

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaPoupanca.java
@@ -4,6 +4,10 @@ package com.dio.banco.Desafio.Dio.Banco.model;
 
 public class ContaPoupanca extends Conta {
 
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
     @Override
     public void imprimirExtrato() {
         System.out.println("=== Extrato Conta Poupança ===");
