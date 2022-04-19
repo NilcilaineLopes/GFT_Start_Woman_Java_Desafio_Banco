843f915d950d74fc8ae75668a279c7f602c16fcd
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 17:47:29 -0300

    Atualiza e adiciona metodo super

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaCorrente.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/ContaCorrente.java
@@ -1,13 +1,9 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
 public class ContaCorrente extends Conta {
 
+
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero);
+    }
 }
