61020ce7cbae47b4a1301cd7afbbc33eafde8d1e
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 19:30:57 -0300

    Adiciona set

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
@@ -6,9 +6,11 @@ public class Banco {
     private String nome;
     private List<Conta> contas;
 
-
-
     public String getNome() {
         return nome;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
     }
 }
