e88b0dd4d0636d744a05aa3c0cdf481e096fe5ff
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 22:09:17 -0300

    Adiciona anotação loombok e ajusta atributo

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Banco.java
@@ -1,14 +1,13 @@
 package com.dio.banco.Desafio.Dio.Banco.model;
 
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
 public class Banco {
     private String nome;
     private List<Conta> contas;
-
-    public String getNome() {
-        return nome;
-    }
 
     public void setContas(List<Conta> contas) {
         this.contas = contas;
