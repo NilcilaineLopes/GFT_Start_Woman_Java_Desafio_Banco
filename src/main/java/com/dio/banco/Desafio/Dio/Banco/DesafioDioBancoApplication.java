38491e8560339291c0d71efa26098433dd7522c9
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 22:08:01 -0300

    Teste de funcionalidades status: ok

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/DesafioDioBancoApplication.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/DesafioDioBancoApplication.java
@@ -1,24 +1,31 @@
 package com.dio.banco.Desafio.Dio.Banco;
 
-import com.dio.banco.Desafio.Dio.Banco.model.Cliente;
-import com.dio.banco.Desafio.Dio.Banco.model.Conta;
-import com.dio.banco.Desafio.Dio.Banco.model.ContaCorrente;
-import com.dio.banco.Desafio.Dio.Banco.model.ContaPoupanca;
-import org.springframework.boot.SpringApplication;
+import com.dio.banco.Desafio.Dio.Banco.model.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DesafioDioBancoApplication {
 
 	public static void main(String[] args) {
-		Cliente nilci = new Cliente();
-		nilci.setNome("Nilcilaine");
+		Cliente cliente = new Cliente();
+		cliente.setNome("Nilcilaine");
+		cliente.setCpf("852.245.556-66");
 
-		Conta cc = new ContaCorrente(nilci);
-		Conta poupanca = new ContaPoupanca(nilci);
+		Boleto boleto = new Boleto();
 
-		cc.depositar(100);
-		cc.tranferir(50,poupanca);
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+
+		cc.depositar(500);
+		cc.tranferir(0,poupanca);
+
+		Boleto agua = new Boleto("58422225111120055",20,"Água");
+		Boleto luz = new Boleto("456465467652456446",80,"Luz");
+
+		cc.pagarBoleto(agua);
+		cc.pagarBoleto(luz);
 
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
