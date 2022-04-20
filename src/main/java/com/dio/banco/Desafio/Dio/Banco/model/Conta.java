acfad22c4aa6dc29a5db0d9f05b5895bc5aded7e
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-19 22:10:28 -0300

    Adiciona metodo pagar boleto

--- src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Conta.java
+++ src/main/java/com/dio/banco/Desafio/Dio/Banco/model/Conta.java
@@ -18,6 +18,7 @@ public abstract class Conta implements IConta {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
         this.cliente = cliente;
+        this.saldo = 0.0;
     }
 
     @Override
@@ -39,10 +40,27 @@ public abstract class Conta implements IConta {
 
     }
 
+    protected boolean possuiSaldo (double valor) {
+        return getSaldo() >= valor;
+    }
+
     protected void imprimirInfosComuns() {
         System.out.println(String.format("Titular: %s", this.cliente.getNome()));
         System.out.println(String.format("Agencia: %d", this.agencia));
         System.out.println(String.format("Número: %d", this.numero));
         System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
+
+    public void pagarBoleto(Boleto boleto){
+        if(possuiSaldo(boleto.getValorBoleto())){
+            sacar(boleto.getValorBoleto());
+            boleto.setPago(true);
+            System.out.println("Boleto de "+boleto.getTipoConta()+" pago com sucesso");
+        }else{
+            System.out.println("Seu saldo é insuficiente para está operação");
+            boleto.setPago(false);
+
+        }
+    }
 }
+
